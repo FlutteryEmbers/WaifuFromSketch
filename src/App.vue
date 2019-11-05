1624ab21c8bb470d4acbed6de41d91d4299a10ad
wushili <wushili@ubuntu>
2019-11-04 22:31:24 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Draw></Draw>
+  </div>
+</template>
+
+<script>
+import Draw from './components/draw'
+
+export default {
+  name: 'app',
+  components: {
+    Draw
+  }
+}
+</script>
